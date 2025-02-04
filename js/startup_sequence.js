6f8789436d74ff751bb0c070faafd45ea9809b71
Quad <jokifish961@gmail.com>
2025-02-04 15:09:34 +0300

    кнопка включения + улучшил текст загрузки

--- js/startup_sequence.js
+++ js/startup_sequence.js
@@ -1,19 +1,20 @@
 let startup_log1 = [
-    [10, " COPYRIGHT IDM COMPUTER SYSTEMS INC. 1990-1997"],
-    [40, "           ---IDM BIOS ver. 1.5---"],
-    [40, ""],
-    [40, "          === C.I.O. 'OBSERVERS' ==="],
-    [40, "    - 'ZASCHITIT' 'OBEZOPASIT' SOHRANIT' -"],
-    [40, " "],
-    [6500, "Loading domain"],
+    [10, " COPYRIGHT IDM COMPUTER SYSTEMS INC. 1990-1997<br>"],
+    [40, "           ---IDM BIOS ver. 1.5---<br>"],
+    [40, "<br>"],
+    [40, "          === C.I.O. 'OBSERVERS' ===<br>"],
+    [40, "    - 'ZASCHITIT' 'OBEZOPASIT' SOHRANIT' -<br>"],
+    [40, "<br>"],
+    [6500, "Initializing...<br>"],
     [2000, "Checking internet connection status..."],
-    [100, "Verification passed"],
-    [300, "Connecting to C.I.O. 'OBSERVERS' repo..."],
-    [800, "Connecting established"],
-    [100, "Scaning src/cio_obs..."],
-    [600, "19840 new packages found"],
-    [200, "Requesting 19840 packages"],
-    [900, "Allocating storage space"],
+    [600, " OK<br>"],
+    [100, "Verification passed<br>"],
+    [300, "Connecting to C.I.O. 'OBSERVERS' repo...<br>"],
+    [800, "Connecting established<br>"],
+    [100, "Scaning src/cio_obs...<br>"],
+    [600, "19840 new packages found<br>"],
+    [200, "Requesting 19840 packages<br>"],
+    [900, "Allocating storage space<br>"],
 ];
 let startup_log2 = [
     [100, "All data downloaded"],
@@ -58,7 +59,7 @@ function begin_startup_sequence() {
     document.getElementById("begin_button").remove();
 
     var console = document.getElementById("main_console");
-    display_strokes_delayed(console, startup_log1);
+    display_strokes_delayed_noline(console, startup_log1);
     let current_time = calc_time(startup_log1);
     current_time += 1000;
     setTimeout(function() {
@@ -96,6 +97,17 @@ function display_strokes_delayed(element, strokes)
         current_time += strokes[i][0];
         setTimeout(function() {
             element.innerHTML += strokes[i][1] + "<br>";
+        }, current_time);
+    }
+}
+
+function display_strokes_delayed_noline(element, strokes)
+{
+    let current_time = 0;
+    for (let i = 0; i < strokes.length; i++) {
+        current_time += strokes[i][0];
+        setTimeout(function() {
+            element.innerHTML += strokes[i][1];
         }, current_time);
     }
 }
